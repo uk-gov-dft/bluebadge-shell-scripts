7f3246edc718363d7c765a50796c0d8f976d67b1
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-02-06 13:20:05 +0000

    Adding a latest tag

--- docker-publish.sh
+++ docker-publish.sh
@@ -20,6 +20,6 @@ docker build -t "$DOCKER_IMAGE_NAME" --build-arg JAR_NAME="$JAR_NAME" .
 
 $(aws ecr get-login --no-include-email --region eu-west-2)
 
-docker tag "$DOCKER_IMAGE_NAME" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
+docker tag "$DOCKER_IMAGE_NAME" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
 
 docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
