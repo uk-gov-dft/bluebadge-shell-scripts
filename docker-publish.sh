a8eae5eff0e39dae66727e502ebc7399fdb7c3c5
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-02-11 10:34:02 +0000

    Tagging with latest

--- docker-publish.sh
+++ docker-publish.sh
@@ -22,13 +22,12 @@ $(aws ecr get-login --no-include-email --region eu-west-2)
 
 docker tag "$DOCKER_IMAGE_NAME" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
 
-docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
-
 if [ "$BRANCH_NAME" == "develop" ] || [ "$BRANCH_NAME" == "push-to-docker-registry"  ]; 
 then
   docker tag "$USER/$REPO:latest" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO:latest"
-  docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO:latest"
 fi
+
+docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
 
 if [ "$BRANCH_NAME" == "master" ]; 
 then
