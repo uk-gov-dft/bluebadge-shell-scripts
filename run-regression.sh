f3ed6acfa71bf1795f67cedb721f9bede74d3a7c
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-12 15:52:13 +0000

    Adding the docker log to the output

--- run-regression.sh
+++ run-regression.sh
@@ -91,8 +91,13 @@ testExitCode=$?
 
 # Save the logs if something went wrong
 # if [[ "$testExitCode" -ne 0 ]]; then
-cd dev-env-$BRANCH_NAME
-docker-compose logs -t --no-color > ../"$JOB_NAME-docker.log"
+if [[ "$testExitCode" -ne 0 ]]; then
+   cd dev-env-$BRANCH_NAME
+   docker-compose logs -t --no-color > ../docker.log
+   cd ..
+fi
+
+
 cd ..
 # fi
 
