14e06b5f7e3298b27d0459db6cd87c425e3316e0
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-13 11:59:55 +0000

    Adding in the rebase script

--- run-regression.sh
+++ run-regression.sh
@@ -81,6 +81,7 @@ docker-compose build
 docker-compose up -d --no-color
 ./wait_for_it.sh localhost:2222 localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health
 psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-users.sql
+psql -h localhost -U developer -d bb_dev -f ./scripts/db/rebase_la.sql -a
 
 ssh-keyscan -p 2222 localhost > ~/.ssh/sftp_known_hosts
 
