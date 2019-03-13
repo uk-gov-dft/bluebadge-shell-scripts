d937b1bdbc57df582b0e7a5db0b30597865fdab7
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-13 16:31:55 +0000

    loading modules

--- run-regression.sh
+++ run-regression.sh
@@ -76,7 +76,7 @@ fi
 outputVersions
 
 cd "dev-env-develop"
-
+bash load-modules.sh
 docker-compose build
 docker-compose up -d --no-color
 ./wait_for_it.sh localhost:2222 localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health
