34e321ccf3f9c2f2e5b110919293b9b0dd51c34a
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-15 11:50:15 +0000

    updating publish script

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 
+$(aws ecr get-login --no-include-email --region eu-west-2)
 
 export PATTERN=".*github.com[\/\:]+([^\/]+)\/([^\.]+)(\.git)?"
 export URL=$(git config --get remote.origin.url)
@@ -22,14 +23,13 @@ then
   docker build -t "$DOCKER_IMAGE_NAME" -t "$USER/$REPO:latest" --build-arg JAR_NAME="$JAR_NAME" .
   docker tag "$USER/$REPO:latest" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO:latest"
   docker tag "$DOCKER_IMAGE_NAME" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
+  docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO:latest"
 else
   docker build -t "$DOCKER_IMAGE_NAME" --build-arg JAR_NAME="$JAR_NAME" .
   docker tag "$DOCKER_IMAGE_NAME" "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
 fi
 
-$(aws ecr get-login --no-include-email --region eu-west-2)
 
 
 docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$DOCKER_IMAGE_NAME"
-docker push "536084723381.dkr.ecr.eu-west-2.amazonaws.com/$USER/$REPO:latest"
 
