65adc42beea653201b13a06fd1d4bac50cc1654f
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-18 17:00:00 +0000

    Getting the docker log on wait for nit failurte

--- run-regression.sh
+++ run-regression.sh
@@ -89,12 +89,9 @@ cd "dev-env-develop"
 bash load-modules.sh
 docker-compose build
 docker-compose up -d --no-color
-{
-  ./wait_for_it.sh localhost:2222 localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health
-} || {
-   docker-compose logs -t --no-color > ../docker.log
-   exit 1
-}
+
+$(./wait_for_it.sh localhost:2222 localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health) || docker-compose logs -t --no-color > ../docker.log
+
 psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-users.sql
 psql -h localhost -U developer -d bb_dev -f ./scripts/db/rebase_la.sql -a
 
